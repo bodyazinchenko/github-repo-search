c33a2333ddbf595cc41f705b362dfa477d13a2be
Bogdan Zinchenko <bodya.9787@gmail.com>
2020-02-18 17:16:43 +0200

    Updates

---
+++ precache-manifest.4bce7951a2f246e34b7c4cd287be51dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25a5b88b63fd82faf2408115da61641",
+    "url": "/github-repo-search/index.html"
+  },
+  {
+    "revision": "de2a76915160678ac1fd",
+    "url": "/github-repo-search/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "48fcdd26cc59834eb03a",
+    "url": "/github-repo-search/static/css/main.082bef2a.chunk.css"
+  },
+  {
+    "revision": "de2a76915160678ac1fd",
+    "url": "/github-repo-search/static/js/2.1b8d85e4.chunk.js"
+  },
+  {
+    "revision": "47a9ed9d77ff510e72664fa282a6d549",
+    "url": "/github-repo-search/static/js/2.1b8d85e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fcdd26cc59834eb03a",
+    "url": "/github-repo-search/static/js/main.f32307a8.chunk.js"
+  },
+  {
+    "revision": "b8627a362ec77f7a1df0",
+    "url": "/github-repo-search/static/js/runtime-main.e4279023.js"
+  },
+  {
+    "revision": "14604b66b3b3c62aa02420c2ce17542f",
+    "url": "/github-repo-search/static/media/star.14604b66.svg"
+  }
+]);
