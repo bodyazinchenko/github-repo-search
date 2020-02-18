8a904f1634d4fb172e90796f2c6633aa3c426f2c
Bogdan Zinchenko <bodya.9787@gmail.com>
2020-02-18 10:37:56 +0200

    Added mobx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure } from 'mobx';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+configure({ enforceActions: 'observed' });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
